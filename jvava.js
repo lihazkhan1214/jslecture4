8da2fbde7751c28036850409da6177aafeb30579
lihazkhan1214 <lehazali123@gmail.com>
2024-01-09 15:50:33 +0500

    add project

---
+++ jvava.js
@@ -0,0 +1,80 @@
+// console.log("functions")
+
+
+// function add(){
+//     console.log("add function")
+// }
+// functions
+// function show(){
+//     console.log("this is function")
+  
+// }
+// function call
+// show();
+// add();
+// parameters 
+// function sub(){
+
+//     let a=12;
+//     let b=4;
+//     console.log(a-b);
+
+// }
+// sub();
+
+
+
+
+// function mul(a,b){
+   
+//     console.log(a*b);
+// }
+
+// mul(12,5);
+// mul(7,5);
+
+// function returnfunction(a,b){
+//     return a*b;
+// }
+
+
+// let result=returnfunction(12,12)
+
+// console.log(result)
+
+// function display(){
+//     return "return message";
+// }
+
+
+// console.log(display())
+
+
+
+// arrow fucntion
+
+// const add=()=>{
+//     console.log("arrow function");
+// }
+// add();
+
+const sub=()=>{
+    console.log("arrow function");
+}
+sub();
+
+// return arrow funcitons
+const returnfunction=(a,b)=>a+b;
+
+
+console.log(returnfunction(12,3))
+
+
+
+// arrow funcitons parameters
+
+
+const mul=(a,b)=>{
+    console.log(a*b);
+}
+mul(12,56);
